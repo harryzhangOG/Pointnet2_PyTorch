6f5bfe2473f284c2682a22fcec4cff2828a35e51
harryzhangOG <43732483+harryzhangOG@users.noreply.github.com>
2021-09-20 17:25:59 -0400

    Update pointnet2_msg_cls.py

--- pointnet2/models/pointnet2_msg_cls.py
+++ pointnet2/models/pointnet2_msg_cls.py
@@ -2,7 +2,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from pointnet2_ops.pointnet2_modules import PointnetSAModule, PointnetSAModuleMSG
+from pointnet2.pointnet2_lib.pointnet2_ops.pointnet2_modules import PointnetSAModule, PointnetSAModuleMSG
 
 from pointnet2.models.pointnet2_ssg_cls import PointNet2ClassificationSSG
 
